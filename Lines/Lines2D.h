5d77dd1b8e9785963724755e41887a0e6dd636da
Muaz <146129680+MuazMoin@users.noreply.github.com>
2024-03-02 22:04:02 +0100

    FIRST COMMIT

---
+++ Lines/Lines2D.h
@@ -0,0 +1,10 @@
+//
+// Created by Muaz Moin on 02/03/2024.
+//
+#include "Point2D.h"
+#ifndef CG_LINES2D_H
+#define CG_LINES2D_H
+
+
+
+#endif //CG_LINES2D_H
---
+++ Lines/Point2D.h
@@ -0,0 +1,21 @@
+//
+// Created by Muaz Moin on 02/03/2024.
+//
+#ifndef CG_POINT2D_H
+#define CG_POINT2D_H
+
+class Point2D {
+public:
+    double x,y;
+
+    Point2D()=default;
+
+    Point2D(double x, double y) {
+        x = x;
+        y = y;
+    }
+
+};
+sxsdds
+
+#endif //CG_POINT2D_H
