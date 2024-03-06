5acdc6a93b66b4d0d0b2e3e7fb70c53d1a753f77
Muaz <146129680+MuazMoin@users.noreply.github.com>
2024-03-06 15:54:07 +0100

    engine V1 and first tranformationmatrix steps

--- TransformationMatrix.cpp
+++ TransformationMatrix.cpp
@@ -1,14 +1,14 @@
-////
-//// Created by Muaz Moin on 06/03/2024.
-////
 //
-//#include "TransformationMatrix.h"
+// Created by Muaz Moin on 06/03/2024.
 //
-//Matrix TransformationMatrix::scalefigure(const double &scale) {
-//    Matrix matrix;
-//    matrix(0,0) = scale;
-//    matrix(1,1) = scale;
-//    matrix(2,2) = scale;
-//    matrix(3,3) = 1;
-//    return matrix;
-//}
+
+#include "TransformationMatrix.h"
+
+Matrix TransformationMatrix::scalefigure(const double &scale) {
+    Matrix matrix;
+    matrix(1,1) = scale;
+    matrix(2,2) = scale;
+    matrix(3,3) = scale;
+    matrix(4,4) = 1;
+    return matrix;
+}
--- TransformationMatrix.h
+++ TransformationMatrix.h
@@ -1,29 +1,30 @@
-////
-//// Created by Muaz Moin on 06/03/2024.
-////
 //
-//#ifndef ENGINE_TRANSFORMATIONMATRIX_H
-//#define ENGINE_TRANSFORMATIONMATRIX_H
+// Created by Muaz Moin on 06/03/2024.
 //
-//#include "3D Lijntekeningen/vector3d.h"
-//
-//class TransformationMatrix {
-//public:
-//    static Matrix scalefigure(const double &scale);
-//
-//    static Matrix translate(const Vector3D &vector3D);
-//
-//    static Matrix rotateX(const double &angle);
-//
-//    static Matrix rotateY(const double &angle);
-//
-//    static Matrix rotateZ(const double &angle);
-//
-//    static Matrix eyePointTransformation(const Vector3D &eyepoint);
-//
-//
-//    static Matrix linedrawing3DTransformation(const double &scale, const double &angleX, const double &angleY, const double &angleZ,
-//                                const Vector3D &vector, Vector3D eye);
-//
-//};
-//#endif //ENGINE_TRANSFORMATIONMATRIX_H
+
+#ifndef ENGINE_TRANSFORMATIONMATRIX_H
+#define ENGINE_TRANSFORMATIONMATRIX_H
+
+#include "3D Lijntekeningen/vector3d.h"
+
+class TransformationMatrix {
+public:
+    static Matrix scalefigure(const double &scale);
+
+    static Matrix translate(const Vector3D &vector3D);
+
+    static Matrix rotateX(const double angle);
+
+    static Matrix rotateY(const double angle);
+
+    static Matrix rotateZ(const double angle);
+
+    static Matrix eyePointTransformation(const Vector3D &eyepoint); //zorgt ervoor dat een 3d object op een 2d vlak getoond wordt.
+
+
+    static Matrix linedrawing3DTransformation(const double &scale, const double &angleX, const double &angleY, const double &angleZ,
+                                const Vector3D &vector, Vector3D eye);
+
+};
+
+#endif //ENGINE_TRANSFORMATIONMATRIX_H
