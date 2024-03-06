3b352660d372422c5d864c2d395010426c30e75c
Muaz <146129680+MuazMoin@users.noreply.github.com>
2024-03-06 03:05:53 +0100

    engine V1 and first tranformationmatrix steps

--- TransformationMatrix.h
+++ TransformationMatrix.h
@@ -5,4 +5,20 @@
 #ifndef ENGINE_TRANSFORMATIONMATRIX_H
 #define ENGINE_TRANSFORMATIONMATRIX_H
 
+#include "3D Lijntekeningen/vector3d.h"
+
+class TransformationMatrix {
+public:
+    static Matrix scalfigure(const double &scale);
+    static Matrix translate(const Vector3D &vector3D);
+
+    static Matrix rotateX(const double &angle);
+    static Matrix rotateY(const double &angle);
+    static Matrix rotateZ(const double &angle);
+
+    static Matrix eyePointTransformation(const Vector3D &eyepoint);
+
+
+};
+
 #endif //ENGINE_TRANSFORMATIONMATRIX_H
