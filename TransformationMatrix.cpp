74bddbd3658b34ee4f537935fcdea6dcdc6c8dc8
Muaz <146129680+MuazMoin@users.noreply.github.com>
2024-03-06 16:16:45 +0100

    engine V1 and Matrix

--- TransformationMatrix.cpp
+++ TransformationMatrix.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "TransformationMatrix.h"
+#include "cmath"
 
 Matrix TransformationMatrix::scalefigure(const double &scale) {
     Matrix matrix;
@@ -12,3 +13,32 @@ Matrix TransformationMatrix::scalefigure(const double &scale) {
     matrix(4,4) = 1;
     return matrix;
 }
+
+Matrix TransformationMatrix::rotateX(const double angle) {
+    Matrix matrix;
+    matrix(2,2) = cos(angle);
+    matrix(2,3) = -sin(angle);
+    matrix(3,2) = sin(angle);
+    matrix(3,3) = cos(angle);
+    return matrix;
+}
+
+Matrix TransformationMatrix::rotateY(const double angle) {
+    Matrix matrix;
+    matrix(1,1) = cos(angle);
+    matrix(1,3) = sin(angle);
+    matrix(3,1) = -sin(angle);
+    matrix(3,3) = cos(angle);
+    return matrix;
+}
+
+Matrix TransformationMatrix::rotateZ(const double angle) {
+    Matrix matrix;
+    matrix(1,1) = cos(angle);
+    matrix(1,2) = -sin(angle);
+    matrix(2,1) = sin(angle);
+    matrix(3,2) = cos(angle);
+    return matrix;
+}
+
+
