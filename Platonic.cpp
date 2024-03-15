945ed7d9cf133b2ea0a7a230f337472e1a570fd7
Muaz <146129680+MuazMoin@users.noreply.github.com>
2024-03-15 19:23:47 +0100

    Opdracht 1 en 2 Working. Started working on opdracht 3

--- Platonic.cpp
+++ Platonic.cpp
@@ -11,7 +11,6 @@
 Figure Platonic::createCube(const std::vector<double> &rotations, const double &scale, const Vector3D &center,
                             const img::Color &color) {
 
-    std::vector<Face> faces;
     std::vector<Vector3D> points;
 
     points.emplace_back(Vector3D::point(1, -1, -1));
@@ -23,15 +22,20 @@ Figure Platonic::createCube(const std::vector<double> &rotations, const double &
     points.emplace_back(Vector3D::point(1, -1, 1));
     points.emplace_back(Vector3D::point(-1, 1, 1));
 
+    std::vector<Face> face;
+
+    face.emplace_back(Face({0, 1, 3, 2})); // Onderste vlak
+    face.emplace_back(Face({1, 5, 7, 3})); // Voorkant vlak
+    face.emplace_back(Face({5, 4, 6, 7})); // Bovenste vlak
+    face.emplace_back(Face({4, 0, 2, 6})); // Achterkant vlak
+    face.emplace_back(Face({2, 3, 7, 6})); // Rechterkant vlak
+    face.emplace_back(Face({0, 4, 5, 1})); // Linkerkant vlak
 
 }
-
-
 
 Figure Platonic::createTetrahedron(const std::vector<double> &rotations, const double &scale, const Vector3D &center,
                                    const img::Color &color) {
 
-    std::vector<Face> faces;
     std::vector<Vector3D> points;
 
     points.emplace_back(Vector3D::point(1, -1, -1));
@@ -39,13 +43,20 @@ Figure Platonic::createTetrahedron(const std::vector<double> &rotations, const d
     points.emplace_back(Vector3D::point(1, 1, 1));
     points.emplace_back(Vector3D::point(-1, -1, 1));
 
+    std::vector<Face> face;
+
+    face.emplace_back(Face({0, 1, 3})); // Eerste vlak
+    face.emplace_back(Face({1, 2, 3})); // Tweede vlak
+    face.emplace_back(Face({0, 3, 2})); // Derde vlak
+    face.emplace_back(Face({0, 2, 1})); // Vierde vlak
+
+
 
 }
 
 Figure Platonic::createOctahedron(const std::vector<double> &rotations, const double &scale, const Vector3D &center,
                                   const img::Color &color) {
 
-    std::vector<Face> faces;
     std::vector<Vector3D> points;
 
     points.emplace_back(Vector3D::point(1, 0, 0));
@@ -55,12 +66,24 @@ Figure Platonic::createOctahedron(const std::vector<double> &rotations, const do
     points.emplace_back(Vector3D::point(0, 0, -1));
     points.emplace_back(Vector3D::point(0, 0, 1));
 
+    std::vector<Face> face;
+
+    face.emplace_back(Face({0, 1, 2})); // Eerste vlak
+    face.emplace_back(Face({0, 2, 3})); // Tweede vlak
+    face.emplace_back(Face({0, 3, 4})); // Derde vlak
+    face.emplace_back(Face({0, 4, 1})); // Vierde vlak
+    face.emplace_back(Face({5, 1, 2})); // Vijfde vlak
+    face.emplace_back(Face({5, 2, 3})); // Zesde vlak
+    face.emplace_back(Face({5, 3, 4})); // Zevende vlak
+    face.emplace_back(Face({5, 4, 1})); // Achtste vlak
+
+
 }
 
 Figure Platonic::createIcosahedron(const std::vector<double> &rotations, const double &scale, const Vector3D &center,
                                    const img::Color &color) {
 
-    std::vector<Face> faces;
+    std::vector<Face> face;
     std::vector<Vector3D> points;
 
 }
@@ -68,6 +91,6 @@ Figure Platonic::createIcosahedron(const std::vector<double> &rotations, const d
 Figure Platonic::createDodecahedron(const std::vector<double> &rotations, const double &scale, const Vector3D &center,
                                     const img::Color &color) {
 
-    std::vector<Face> faces;
+    std::vector<Face> face;
     std::vector<Vector3D> points;
 }
