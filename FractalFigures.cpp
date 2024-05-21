88567d0679f9f1d6be9fd1be44ec3482626924ce
Muaz <146129680+MuazMoin@users.noreply.github.com>
2024-05-21 13:45:52 +0200

    engine working perfectly

--- FractalFigures.cpp
+++ FractalFigures.cpp
@@ -2,22 +2,25 @@
 #include "Header Files/TransformationMatrix.h"
 
 Figures3d FractalFigures::generateFractal(Figure &figure, int nrIterations, double fractalScale) {
-            Figures3d resultingFigures = {figure};
+    Figures3d resultingFigures = {figure};
 
     if (nrIterations == 0) {
         return resultingFigures;
     } else {
         for (int i = 0; i < nrIterations; ++i) {
             Figures3d newFigures;
-            for (const Figure& figure : resultingFigures) {
-                for (int j = 0; j < figure.points.size(); ++j) {
-                    Figure newFigure = figure;
-                    newFigure.center = figure.points[j];
-                    newFigure.scale = figure.scale / fractalScale;
+            for (const Figure& baseFigure : resultingFigures) {
+                for (int j = 0; j < baseFigure.points.size(); ++j) {
+                    Figure newFigure = baseFigure;
+                    newFigure.applyTransformation(TransformationMatrix::scalefigure(1 / fractalScale));
+
+                    Vector3D originalPoint = baseFigure.points[j];
+                    Vector3D scaledPoint = newFigure.points[j];
+
+                    newFigure.applyTransformation(TransformationMatrix::translate(originalPoint - scaledPoint));
                     newFigures.push_back(newFigure);
                 }
             }
-
             resultingFigures = newFigures;
         }
         return resultingFigures;
